47daecb399f953c65d40755e955f406309f22dbb
silvante <khamidov.ko@gmail.com>
2025-02-15 13:35:02 +0500

    form data

--- src/components/UserSettings.jsx
+++ src/components/UserSettings.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const UserSettings = ({userProfile}) => {
-  const [imageBase64, setImageBase64] = useState(null);
+const UserSettings = ({ userProfile }) => {
+  const [imageBase64, setImageBase64] = useState(userProfile.avatar && userProfile.avatar.small);
 
-  const [name, setname] = useState(userProfile.name)
-  const [bio, setbio] = useState(userProfile.bio)
-  const [avatar, setavatar] = useState(userProfile.avatar)
-  const [username, setusername] = useState(userProfile.username)
+  const [name, setname] = useState(userProfile.name);
+  const [bio, setbio] = useState(userProfile.bio || "");
+  const [avatar, setavatar] = useState(userProfile.avatar && userProfile.avatar.small );
+  const [username, setusername] = useState(userProfile.username);
 
-
+  console.log(name, bio, username, avatar);
+  
 
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
@@ -16,7 +17,7 @@ const UserSettings = ({userProfile}) => {
 
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    
+
     reader.onload = () => {
       setImageBase64(reader.result);
       uploadToServer(reader.result);
@@ -28,43 +29,77 @@ const UserSettings = ({userProfile}) => {
   };
 
   return (
-    <div className='flex-1 bg-white rounded-3xl border p-5'>
-        <h2 className='text-xl mb-5'>Malumotlarni sozlash</h2>
-        <form className='space-y-5'>
-          <div className='w-full space-y-1'>
-            <p>Profil uchun rasm*</p>
-            <label htmlFor="base64" className='bg-gray-300 w-24 h-24 flex items-center justify-center rounded-xl overflow-hidden'>
-              {
-                imageBase64 ? (
-                  <img src={imageBase64} alt="base64" className='w-full h-full aspect-square object-cover' />
-                ) : (
-                  <i className='bx bx-image-add text-3xl'></i>
-                )
-              }
-            </label>
-            <input type="file" id='base64' accept='image/*' onChange={handleImageUpload} className='hidden' />
+    <div className="flex-1 bg-white rounded-3xl border p-5">
+      <h2 className="text-xl mb-5">Malumotlarni sozlash</h2>
+      <form className="space-y-5">
+        <div className="w-full space-y-1">
+          <p>Profil uchun rasm*</p>
+          <label
+            htmlFor="base64"
+            className="bg-gray-300 w-24 h-24 flex items-center justify-center rounded-xl overflow-hidden"
+          >
+            {imageBase64 ? (
+              <img
+                src={imageBase64}
+                alt="base64"
+                className="w-full h-full aspect-square object-cover"
+              />
+            ) : (
+              <i className="bx bx-image-add text-3xl"></i>
+            )}
+          </label>
+          <input
+            type="file"
+            id="base64"
+            accept="image/*"
+            onChange={handleImageUpload}
+            className="hidden"
+          />
+        </div>
+        <div className="w-full space-y-1">
+          <label htmlFor="bio">Shaxsiy bio*</label>
+          <input
+            type="text"
+            id="bio"
+            value={bio}
+            onChange={(e) => setbio(e.target.value)}
+            className="bg-gray-200 border outline-none py-3 px-5"
+            placeholder="maximum 225 ta harf"
+          />
+        </div>
+        <div className="flex gap-5">
+          <div className="flex-1 space-y-1">
+            <label htmlFor="name">ismni taxrirlash*</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setname(e.target.value)}
+              className="bg-gray-200 border outline-none py-3 px-5"
+              placeholder="maximum 225 ta harf"
+            />
           </div>
-          <div className='w-full space-y-1'>
-            <label htmlFor="bio">Shaxsiy bio*</label>
-            <input type="text" id="bio" className='bg-gray-200 border outline-none py-3 px-5' placeholder='maximum 225 ta harf' />
+          <div className="flex-1 space-y-1">
+            <label htmlFor="username">foidalanuvchi nomini taxrirlash*</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setusername(e.target.value)}
+              className="bg-gray-200 border outline-none py-3 px-5"
+              placeholder="maximum 225 ta harf"
+            />
           </div>
-          <div className='flex gap-5'>
-            <div className='flex-1 space-y-1'>
-              <label htmlFor="name">ismni taxrirlash*</label>
-              <input type="text" id="name" className='bg-gray-200 border outline-none py-3 px-5' placeholder='maximum 225 ta harf' />
-            </div>
-            <div className='flex-1 space-y-1'>
-              <label htmlFor="username">foidalanuvchi nomini taxrirlash*</label>
-              <input type="text" id="username" className='bg-gray-200 border outline-none py-3 px-5' placeholder='maximum 225 ta harf' />
-            </div>
-          </div>
-          <div className='flex flex-col justify-start space-y-1'>
-            <label>malumotlarni saqlash*</label>
-            <button type='submit' className='btn-primary py-2 px-5 w-56'>O'zgarishlarni saqlash</button>
-          </div>
-        </form>
+        </div>
+        <div className="flex flex-col justify-start space-y-1">
+          <label>malumotlarni saqlash*</label>
+          <button type="submit" className="btn-primary py-2 px-5 w-56">
+            O'zgarishlarni saqlash
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default UserSettings
+export default UserSettings;
